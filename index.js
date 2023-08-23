ee1148538e19700c242ec76807e0501d903bb73c
Kris Wong <krischwong@gmail.com>
2023-08-22 21:24:39 -0400

    Created skeleton

---
+++ index.js
@@ -0,0 +1,12 @@
+const Node = () => {
+  const data = null;
+  const left = null;
+  const right = null;
+};
+
+const Tree = () => {
+  let root = null;
+
+  const buildTree = () => {};
+  return { root, buildTree };
+};
