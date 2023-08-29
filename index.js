6b520af50d0d8784755818643cf3588c4cbaa423
Kris Wong <krischwong@gmail.com>
2023-08-29 19:24:24 -0400

    added buildTree function to sort and remove duplicates in array

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const Node = () => {
 const Tree = () => {
   let root = null;
 
-  const buildTree = () => {};
+  const buildTree = (arr) => {
+    const newArr = arr.sort((a, b) => a - b);
+    return [...new Set(newArr)];
+  };
   return { root, buildTree };
 };
+
+const nt = Tree();
+
+console.log(nt.buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]));
