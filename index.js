669ac0b7b28221f0b84fa528c528b2cdf7ecb3c5
Kris Wong <krischwong@gmail.com>
2023-08-30 01:09:57 -0400

    finished buildTree function

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
-const Node = () => {
-  const data = null;
-  const left = null;
-  const right = null;
+//Node factory that creates an object with three values. The actual value itself and its two children.
+const Node = (value) => {
+  const data = value;
+  let left = null;
+  let right = null;
+  return { data, left, right };
 };
 
-const Tree = () => {
+//Tree factory that creates the BST based on array provided. First sorts it then removes duplicates.
+const Tree = (array) => {
   let root = null;
+  const sortedArr = [...new Set(array.sort((a, b) => a - b))];
+  const first = 0;
+  const last = sortedArr.length - 1;
 
-  const buildTree = (arr) => {
-    const newArr = arr.sort((a, b) => a - b);
-    return [...new Set(newArr)];
+  //Builds the node tree and returns level 0 root node
+  const buildTree = (arr, start, end) => {
+    //This step is important because it sets the new mid point as the middle point of the right/left arrays.
+    //Math.ceil/floor to remove decimals.
+    const mid = Math.ceil((start + end) / 2);
+    if (start > end) {
+      return null;
+    }
+
+    //Preorder traversal
+    let node = Node(arr[mid]);
+    node.left = buildTree(arr, start, mid - 1);
+    node.right = buildTree(arr, mid + 1, end);
+    return node;
   };
+
+  const insert = () => {};
+
+  const delete2 = () => {};
+
+  root = buildTree(sortedArr, first, last);
   return { root, buildTree };
 };
 
-const nt = Tree();
+const nt = Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 
-console.log(nt.buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]));
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+console.log(prettyPrint(nt.root));
