fac935c14cc3b275b77f2fc6eb5e9189e3896da8
Kris Wong <krischwong@gmail.com>
2023-08-30 22:19:11 -0400

    implemented insert function

--- index.js
+++ index.js
@@ -29,12 +29,40 @@ const Tree = (array) => {
     return node;
   };
 
-  const insert = () => {};
+  //Function to find the appropriate leaf to insert a node on. The function first creates a note object with the value provided
+  //The value of the root node is then compared with the provided value.
+  //If the provided value is less than the node value, it will go to the left child node. If it is greater, it will go to the right node.
+  //This process will repeat until there are no child nodes left (the node being compared is the leaf node).
+  //The created note will then be appended to the left or right of the leaf node depending on its value.
+  const insert = (value) => {
+    let newNode = Node(value);
+
+    const search = (node) => {
+      if (value < node.data) {
+        //If there is no left node, newNode will be appended
+        if (!node.left) {
+          node.left = newNode;
+        } else {
+          //Recursively call itself to check child node.
+          search(node.left);
+        }
+      } else if (value > node.data) {
+        //If there is no right node, newNode will be appended
+        if (!node.right) {
+          node.right = newNode;
+        } else {
+          //Recursively call itself to check child node.
+          search(node.right);
+        }
+      }
+    };
+    search(root); //Initial call of the rule node
+  };
 
   const delete2 = () => {};
 
   root = buildTree(sortedArr, first, last);
-  return { root, buildTree };
+  return { root, buildTree, insert };
 };
 
 const nt = Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
@@ -52,4 +80,5 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   }
 };
 
-console.log(prettyPrint(nt.root));
+nt.insert(67);
+prettyPrint(nt.root);
