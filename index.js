e815767cd9d9c47362dc965cbeb18b6e741048b5
Kris Wong <krischwong@gmail.com>
2023-09-21 22:59:32 -0400

    Added delete function

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const Tree = (array) => {
 
   //Builds the node tree and returns level 0 root node
   const buildTree = (arr, start, end) => {
-    //This step is important because it sets the new mid point as the middle point of the right/left arrays.
+    //This step is important because it sets the new mid point as the middle point of each new subarray.
     //Math.ceil/floor to remove decimals.
     const mid = Math.ceil((start + end) / 2);
     if (start > end) {
@@ -56,13 +56,79 @@ const Tree = (array) => {
         }
       }
     };
-    search(root); //Initial call of the rule node
+    search(root); //Initial call of the root node
   };
 
-  const delete2 = () => {};
+  //Finds the min value of the whole tree
+  const min = () => {
+    let currentNode = root;
+
+    while (currentNode.left) {
+      currentNode = currentNode.left;
+    }
+
+    return currentNode.data;
+  };
+
+  //Finds the min value of a given subsection of the tree
+  const minNode = (node) => {
+    let currentNode = node;
+
+    while (currentNode.left) {
+      currentNode = currentNode.left;
+    }
+
+    return currentNode.data;
+  };
+
+  //Function to delete a node given a specific value. Like the insert function, it will search the tree base by comparing the value and the data of the node.
+  //Lesser = Left, Greater = Right.
+  //If the value matches, it will then check one of three conditions.
+  //If it's a leaf node (no children), it will simply set the parent node to have a null value instead of a link to the deleted node
+  //If there is a single child, the specified node will have it's value be replaced by the data of it's child and the link to the child will be set to null.
+  //If there is two children, the minimum data of the right subtree will replace the data of the target value.
+  const deleteNode = (value) => {
+    //The search function that traverses the true with a provided node and a value to look for.
+    const search = (currentNode, value) => {
+      if (value < currentNode.data) {
+        //By setting the left and right of the currentNode to be the return of the search function, when the function resolves, it will be replaced by the same node
+        //if the search value is greater or lesser than the data value.
+        currentNode.left = search(currentNode.left, value);
+        return currentNode;
+      } else if (value > currentNode.data) {
+        currentNode.right = search(currentNode.right, value);
+        return currentNode;
+      }
+      //Leaf Node
+      if (!currentNode.left && !currentNode.right) {
+        return null;
+      }
+      //One child
+      if (!currentNode.right) {
+        let temporaryNode = currentNode.left;
+        currentNode.left = null;
+        return temporaryNode;
+      } else if (!currentNode.left) {
+        let temporaryNode = currentNode.right;
+        currentNode.right = null;
+        return temporaryNode;
+      }
+      //If the node has both children. It first takes the right subtree of the node to be deleted then finds the minimum value of the subtree and saves it.
+      //It then calls the search function recursively on the minimum value of the right subtree, thereby removing it.
+      //The currentNode data is then set to be the saved minimum value of the right subtree. The node is then returned.
+      if (currentNode.right && currentNode.left) {
+        let rightSide = currentNode.right;
+        let rightMin = minNode(rightSide);
+        search(currentNode, rightMin);
+        currentNode.data = rightMin;
+        return currentNode;
+      }
+    };
+    search(root, value);
+  };
 
   root = buildTree(sortedArr, first, last);
-  return { root, buildTree, insert };
+  return { root, buildTree, insert, min, deleteNode };
 };
 
 const nt = Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
@@ -80,5 +146,8 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   }
 };
 
-nt.insert(67);
 prettyPrint(nt.root);
+// nt.insert(66);
+// prettyPrint(nt.root);
+// nt.deleteNode(67);
+// prettyPrint(nt.root);
